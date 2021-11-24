be9352c1fe00842c1c4478564c54004df74b084f
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2021-11-24 12:25:51 -0600

    adds code to construct html and add astronauts to page

--- script.js
+++ script.js
@@ -2,8 +2,15 @@ window.addEventListener("load", function () {
   this.fetch(
     "https://handlers.education.launchcode.org/static/astronauts.json"
   ).then(function (response) {
-    response.json().then(function (json) {
-      console.log(json[0]);
+    return response.json().then(function (json) {
+      let html = "";
+      const container = document.getElementById("container");
+      for (let i = 0; i < json.length; i++) {
+        html =
+          html +
+          `<div class='astronaut'><div class='bio'><h3>${json[i].firstName} ${json[i].lastName}</h3><ul><li>Hours in space: ${json[i].hoursInSpace}</li><li>Active: ${json[i].active}</li><li>Skills: ${json[i].skills}</li></ul></div><img class='avatar' src='${json[i].picture}'></div>`;
+      }
+      container.innerHTML = html;
     });
   });
 });
