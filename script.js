7dad5fed1930b678013ec3918d89cba83d28c64c
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2021-11-24 15:46:33 -0600

    completes studio exercises

--- script.js
+++ script.js
@@ -3,12 +3,22 @@ window.addEventListener("load", function () {
     "https://handlers.education.launchcode.org/static/astronauts.json"
   ).then(function (response) {
     return response.json().then(function (json) {
-      let html = "";
+      let html = `<h2>Total astronauts: ${json.length}</h2>`;
       const container = document.getElementById("container");
+      json.sort(function (a, b) {
+        return b.hoursInSpace - a.hoursInSpace;
+      });
+
       for (let i = 0; i < json.length; i++) {
-        html =
-          html +
-          `<div class='astronaut'><div class='bio'><h3>${json[i].firstName} ${json[i].lastName}</h3><ul><li>Hours in space: ${json[i].hoursInSpace}</li><li>Active: ${json[i].active}</li><li>Skills: ${json[i].skills}</li></ul></div><img class='avatar' src='${json[i].picture}'></div>`;
+        if (json[i].active === true) {
+          html =
+            html +
+            `<div class='astronaut'><div class='bio'><h3>${json[i].firstName} ${json[i].lastName}</h3><ul><li>Hours in space: ${json[i].hoursInSpace}</li><li class='greenText'>Active: ${json[i].active}</li><li>Skills: ${json[i].skills}</li></ul></div><img class='avatar' src='${json[i].picture}'></div>`;
+        } else {
+          html =
+            html +
+            `<div class='astronaut'><div class='bio'><h3>${json[i].firstName} ${json[i].lastName}</h3><ul><li>Hours in space: ${json[i].hoursInSpace}</li><li>Active: ${json[i].active}</li><li>Skills: ${json[i].skills}</li></ul></div><img class='avatar' src='${json[i].picture}'></div>`;
+        }
       }
       container.innerHTML = html;
     });
